193817eb058e9f29e515eae1cdfab0ed1a1f1603
mariosancheza <ing_mrsa@hotmail.com>
2021-05-25 01:05:23 -0500

    Clone inicial del proyecto
    Proyecto clonado para revisión

---
+++ Prueba_3/recursos.js
@@ -0,0 +1,50 @@
+function crear(data2){
+    console.log(data2);
+    var col = 3;
+    var filas = 20;
+    var tabla="<table border=\"0\">"
+    for(i=0;i<filas;i++)
+    {
+        tabla+="<tr>";
+        for(j=0;j<col;j++)
+        {
+            if (j == 0)
+            {
+                tabla+="<td>"+"<p>" + data2[i][j] + "<\p>" + "</td>";
+            }
+            if (j == 1)
+            {
+                tabla+="<Td>"+"<p>" + data[i].Publisher + "<\p>"+ "</td>";
+            }
+            if (j == 2)
+            {
+                tabla+="<td>"+"<p>" + data2[i][alter_ego] + "<\p>"+ "</td>";
+            }
+            
+        }
+        tabla+="</tr>";
+    }
+    tabla+="</table>";
+    document.getElementById("root").innerHTML=tabla;
+}
+
+function crear2(){
+    readTextFile("superher0es.json", function(text){
+        var data = JSON.parse(text);
+        //console.log(data);
+        crear(data);
+    });
+
+}
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest()
+    rawFile.overrideMimeType("Application/Json");
+    rawFile.open("Get", file, true)
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(raWFile.responseText);
+        }
+    }
+    rawFile.Send(null);
+}
